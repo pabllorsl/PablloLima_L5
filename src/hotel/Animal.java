ef0590936f97892c03964bd74282b40ad9f5fc09
Pabllo Lima <pabllo.lima@ccc.ufcg.edu.br>
2016-08-09 20:19:23 -0300

    Classe Animal criada

--- src/hotel/Animal.java
+++ src/hotel/Animal.java
@@ -2,4 +2,59 @@ package hotel;
 
 public class Animal {
 
+	private String nome;
+	private String tipo;
+	private int idade;
+
+	public Animal(String nome, String tipo, int idade) {
+		this.nome = nome;
+		this.tipo = tipo;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object objeto) {
+		if (!(objeto instanceof Animal)) {
+			return false;
+		}
+
+		Animal animal = (Animal) objeto;
+
+		return this.getNome().equalsIgnoreCase(animal.getNome()) && this.getTipo().equalsIgnoreCase(animal.getTipo());
+
+	}
+
 }
